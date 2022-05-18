f93982ffb90c0408e8f10587d612f1f475921090
Maclenn77 <maclenn77@gmail.com>
2022-05-17 23:39:36 -0500

    Add validations to books

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,5 @@
 class Book < ApplicationRecord
-    validates :isbn, presence:true
+    validates :isbn, presence:true, length: { is:10 || is:13 }
+    validates :author, presence:true
+    validates :title, presence:true
 end
